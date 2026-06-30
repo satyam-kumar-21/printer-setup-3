cfca6391099a409186ffa1d6fa4a4f855c73ae9b
Satyam kumar <satyamkumar2716@gmail.com>
2026-06-30 23:31:51 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -11,21 +11,21 @@ function Page() {
             <div className="flex flex-wrap items-center justify-center gap-4">
                 <Link
                     href="/printer-setup"
-                    className="rounded-lg bg-blue-600 px-6 py-3 text-white font-medium transition hover:bg-blue-700"
+                    className="rounded-lg bg-blue-600 px-6 py-3 text-white font-medium transition hover:bg-blue-900"
                 >
                     Setup Theme 1
                 </Link>
 
                 <Link
                     href="/printer-setup2"
-                    className="rounded-lg bg-blue-600 px-6 py-3 text-white font-medium transition hover:bg-green-700"
+                    className="rounded-lg bg-blue-600 px-6 py-3 text-white font-medium transition hover:bg-blue-900"
                 >
                     Setup Theme 2
                 </Link>
 
                 <Link
                     href="/printer-setup3"
-                    className="rounded-lg bg-blue-600 px-6 py-3 text-white font-medium transition hover:bg-purple-700"
+                    className="rounded-lg bg-blue-600 px-6 py-3 text-white font-medium transition hover:bg-blue-900"
                 >
                     Setup Theme 3
                 </Link>
